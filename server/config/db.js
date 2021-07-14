168e6a0a8706d249b4368c3a96088cacb8e54290
Sapna <sapna24011996@gmail.com>
2021-07-14 17:54:17 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb+srv://kR4J2fQkrVITNHYR:kR4J2fQkrVITNHYR@booksdb.putqb.mongodb.net/sapna?retryWrites=true&w=majority"
+  "URI": "mongodb://129.213.118.73:27025/sapna-books"
+};
